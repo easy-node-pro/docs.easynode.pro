32742f22a718553e6318fbad74bde24549b14fea
patrickmogul <patrick@easynode.pro>
2025-02-24 23:09:50 -0600

    Initial push

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+import { NotFoundPage } from 'nextra-theme-docs'
+ 
+export default function NotFound() {
+  return (
+    <NotFoundPage content="Submit an issue" labels="broken-link">
+      <h1>The page is not found</h1>
+      <p>Try the main page [here](/).</p>
+    </NotFoundPage>
+  )
+}
--- packages/nextra/src/client/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { useMounted } from './use-mounted.js'
-export { useFSRoute } from './use-fs-route.js'
-export { useHash } from './use-hash.js'
